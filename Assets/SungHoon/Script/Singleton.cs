37e8ecd6f8894786bc52a757908bcc5de9ef7266
OddEye <110804298+Odd3y3@users.noreply.github.com>
2023-08-28 14:27:51 +0900

    merge 에러 수정 함. 다들 Pull 받고 작업해주세요

--- Assets/SungHoon/Script/Singleton.cs
+++ Assets/SungHoon/Script/Singleton.cs
@@ -4,35 +4,6 @@ using UnityEngine;
 
 public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-<<<<<<< HEAD
-	static T _inst = null;
-	public static T Inst
-	{
-		get
-		{
-			if (_inst == null)
-			{
-				_inst = FindObjectOfType<T>();
-				if (_inst == null)
-				{
-					GameObject obj = new GameObject();
-					obj.name = typeof(T).ToString();
-					_inst = obj.AddComponent<T>();
-				}
-			}
-			return _inst;
-		}
-	}
-	protected void Initialized()
-	{
-		if (_inst != null && _inst !=this)
-		{
-			Destroy(_inst.gameObject);
-			_inst = this as T;
-		}
-		DontDestroyOnLoad(transform.root.gameObject);
-	}
-=======
     static T _inst = null;
     public static T Inst
     {
@@ -61,5 +32,4 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
         }
         DontDestroyOnLoad(gameObject);
     }
->>>>>>> a1bf179d72bdacba5fcc1e7841ae693c76374262
 }
